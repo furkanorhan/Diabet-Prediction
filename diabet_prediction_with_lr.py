1e76afed4d45296938fdc68b838874a88cba12d5
Furkan Orhan <66881206+furkanorhan@users.noreply.github.com>
2022-12-27 04:21:11 +0300

    Update diabet_prediction_with_lr.py

--- diabet_prediction_with_lr.py
+++ diabet_prediction_with_lr.py
@@ -2,28 +2,16 @@
 # Diabetes Prediction with Logistic Regression
 ######################################################
 
-# İş Problemi:
-
-# Özellikleri belirtildiğinde kişilerin diyabet hastası olup
-# olmadıklarını tahmin edebilecek bir makine öğrenmesi
-# modeli geliştirebilir misiniz?
-
-# Veri seti ABD'deki Ulusal Diyabet-Sindirim-Böbrek Hastalıkları Enstitüleri'nde tutulan büyük veri setinin
-# parçasıdır. ABD'deki Arizona Eyaleti'nin en büyük 5. şehri olan Phoenix şehrinde yaşayan 21 yaş ve üzerinde olan
-# Pima Indian kadınları üzerinde yapılan diyabet araştırması için kullanılan verilerdir. 768 gözlem ve 8 sayısal
-# bağımsız değişkenden oluşmaktadır. Hedef değişken "outcome" olarak belirtilmiş olup; 1 diyabet test sonucunun
-# pozitif oluşunu, 0 ise negatif oluşunu belirtmektedir.
-
-# Değişkenler
-# Pregnancies: Hamilelik sayısı
-# Glucose: Glikoz.
-# BloodPressure: Kan basıncı.
-# SkinThickness: Cilt Kalınlığı
-# Insulin: İnsülin.
-# BMI: Beden kitle indeksi.
-# DiabetesPedigreeFunction: Soyumuzdaki kişilere göre diyabet olma ihtimalimizi hesaplayan bir fonksiyon.
-# Age: Yaş (yıl)
-# Outcome: Kişinin diyabet olup olmadığı bilgisi. Hastalığa sahip (1) ya da değil (0)
+## Variables
+# Pregnancies
+# Glucose
+# BloodPressure
+# SkinThickness
+# Insulin
+# BMI
+# DiabetesPedigreeFunction
+# Age
+# Outcome- Target Variable
 
 
 # 1. Exploratory Data Analysis
